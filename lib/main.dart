a496422a735c530926114dd91c6e8b6f4508db41
Ramyaeaswaran <ramyaeaswaran77@gmail.com>
2019-09-30 09:53:06 +0530

    exercise 1 and 2

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,25 @@ void main() {
   // Modify this code to make the app look like this: https://github.com/McLarenCollege/Flutter-Course-Notes/blob/master/screenshots/day_1_assignment.png
   // Link to icon: https://mclarencollege.in/images/icon.png
   // Link to center image: https://mclarencollege.in/images/9fc76a72-5b06-432f-b92e-d41d8ad5629f.jpg
-
-  String msg = 'Hello World';
-  Text textWidget = Text(msg);
-  Center center = Center(child: textWidget);
-  MaterialApp myApp = MaterialApp(home: center);
-  runApp(myApp);
-}
+  runApp(
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+       leading: Image(
+        image: NetworkImage('https://mclarencollege.in/images/icon.png')),
+        title: Text('I Love Flutter'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Image(
+            image: NetworkImage(
+                'https://mclarencollege.in/images/9fc76a72-5b06-432f-b92e-d41d8ad5629f.jpg')),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Image(
+            image: NetworkImage('https://mclarencollege.in/images/icon.png'),
+          )
+      )
+    ),
+  ));
+  }
